b7ce35f618fbde677f527adb023fb6155b7dbedb
cbrown5 <c.j.brown@utas.edu.au>
2025-08-19 14:34:30 +1000

    add scripts for GLM results analysis

--- Scripts/results/analyse-glm.R
+++ Scripts/results/analyse-glm.R
@@ -3,6 +3,83 @@
 
 # Richard to make plots here. Below are the files to use. 
 
+library(tidyverse)
+library(readr)
+class_levels <- read_csv("Shared/Data/glm-class-levels.csv")
+question_types <- read_csv("Shared/Data/glm-question-types.csv")
+test_case_results <- read_csv("Shared/Data/glm-test-case-results.csv")
+
+
+#Get maximum for each question type
+test_case_results %>%
+    pivot_longer(cols = -c(1:8, 30), 
+                 names_to = "Question", 
+                 values_to = "Score") %>%
+    left_join(question_types, by = "Question") %>%
+    group_by( Question) %>%
+    summarize(Max_value = max(Score, na.rm = TRUE)) %>%
+    data.frame()
+
+#
+# Pivot longer the results so that each question is a column, and each model is a row.
+#
+test_results <- test_case_results %>%
+    pivot_longer(cols = -c(1:8, 30), 
+                 names_to = "Question", 
+                 values_to = "Score") %>%
+    left_join(question_types, by = "Question") %>%
+    #normalize scores by the max value for each question type
+    mutate(Score_norm = Score / `Max value`)
+
+
+# View(test_results)
+
+names(test_results)
+
+mean_scores <- test_results %>%
+    group_by(Model, Question, Type) %>%
+    summarise(Score_norm = mean(Score_norm, na.rm = TRUE))
+
+ggplot(mean_scores) + 
+    aes(x = Question, y = Model, fill = Score_norm) +
+    geom_tile() +
+    scale_fill_gradient(low = "white", high = "green") +
+    theme(
+        axis.text.x = element_text(angle = 45, hjust = 1),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank()
+    ) 
+
+mean_scores_type <- test_results %>%
+    group_by(Model, Type, folder_name) %>%
+    summarise(Score_norm = mean(Score_norm, na.rm = TRUE)) %>%
+    ungroup() %>% 
+    group_by(Model, Type) %>%
+    summarise(
+        Average = mean(Score_norm, na.rm = TRUE), 
+        Aptitude = quantile(Score_norm, 0.9, na.rm = TRUE),
+        Consistency = 1 - (quantile(Score_norm, 0.9, na.rm = TRUE) - quantile(Score_norm, 0.1, na.rm = TRUE))) %>%
+    ungroup() %>%
+    pivot_longer(cols = c(Average, Aptitude, Consistency), 
+                 names_to = "Metric", 
+                 values_to = "Score_norm") 
+                 
+                 
+ggplot(mean_scores_type) + 
+                    aes(x = Type, y = Model, fill = Score_norm, label = round(Score_norm, 2)) +
+                    geom_tile() +
+                    geom_text(size = 3) +
+                    facet_grid(Metric~ .) +
+                    scale_fill_gradient(low = "white", high = "green") +
+                    theme(
+                        axis.text.x = element_text(angle = 45, hjust = 1),
+                        panel.background = element_blank(),
+                        panel.grid.major = element_blank(),
+                        panel.grid.minor = element_blank()
+                    )
+
+
 #  Questions for the rubric along with levels of answers. This is used to evaluate the AI's performance on the GLM test case.
 `glm-class-levels.csv`
 
